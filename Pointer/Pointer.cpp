cdb440bdc33f6e501a6008bee86b6abb32c53934
verss21 <arjunafrizal01@gmail.com>
2024-05-06 10:51:11 +0700

    template awal

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+	public:
+	int nim;
+	void showNim() {
+		cout << "No Induk = " << nim << endl;
+	}
+};
 
+int main() {
+	mahasiswa mhs{ 1 }; // object mhs
+	mhs.showNim(); // member access operator
+
+	mahasiswa& refMhs = mhs; // pointer reference refMhs
+	refMhs.nim = 2; //member access operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer dereference pMhs
+	pMhs->nim = 3; // arrow operator
+	mhs.showNim();
+	return 0;
+}
