127ff274875e5c073bcfc4dc1723d75c5f2e67c4
verss21 <arjunafrizal01@gmail.com>
2024-05-06 10:55:36 +0700

    display all dan set nama

--- Pointer/friendclass.cpp
+++ Pointer/friendclass.cpp
@@ -21,3 +21,14 @@ public:
 	void setId(int pId);
 	void displayAll(orang& a);
 };
+
+void siswa::displayAll(orang& a)
+{
+	cout << id << endl
+		<< a.nama;
+}
+void orang::setNama(string pNama)
+{
+	nama = pNama;
+}
+void siswa::setId(int pId)
